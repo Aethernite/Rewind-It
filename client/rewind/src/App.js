d561402cdc9bedf4ff8c6095c5693a6cb651ab81
Velizar Nachev <velizar.nachev@mentormate.com>
2020-10-01 16:56:58 +0300

    Added CreateTimesheet page and styled it

--- client/rewind/src/App.js
+++ client/rewind/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import {RegisterPage} from "./components/pages/auth/RegisterPage";
 import LoginPage from "./components/pages/auth/LoginPage";
+import {CreateTimesheet} from "./components/pages/timesheets/CreateTimesheet";
 
 function App() {
     return (
         <div>
-            <LoginPage/>
+            {/*<LoginPage/>*/}
             {/*<RegisterPage/>*/}
+            <CreateTimesheet/>
         </div>
     );
 }
