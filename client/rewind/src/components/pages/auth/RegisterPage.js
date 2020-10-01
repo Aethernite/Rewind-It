30b68d7d2ceb5d4c2810ad02d120c62fe73fb918
Velizar Nachev <velizar.nachev@mentormate.com>
2020-10-01 10:28:21 +0300

    Fixed responsiveness

--- client/rewind/src/components/pages/auth/RegisterPage.js
+++ client/rewind/src/components/pages/auth/RegisterPage.js
@@ -10,8 +10,9 @@ import {RegisterValidationSchema} from "../../../validations/schemas/RegisterVal
 const Title = styled.h1`
   font-family: 'Lobster', cursive;
   letter-spacing: 0.2rem;
-  font-size: 4rem;
-  color: #f0eeee;
+  font-size: 3em;
+  //color: #f0eeee;
+  color: #2e2e2e;
 `
 
 const Label = styled.label`
@@ -43,6 +44,21 @@ const Form = styled.form`
      }
 `;
 
+
+
+const Button = styled.button`
+  height: 2.50rem;
+  border-radius: 10px;
+  border-color: #FFFFFF;
+  outline: none;
+  border-style: none;
+  transition: border 0.3s ease;
+  &:hover{
+  border: 2px solid #81D9FF;
+  cursor: pointer;
+  }
+`
+
 const Header = styled.h2`
 font-family: 'Roboto', sans-serif;
 font-weight: 300;
@@ -58,10 +74,10 @@ export const RegisterPage = () => {
                 <div className="wave"/>
                 <div className="wave"/>
             </div>
-            <Col className="col-md-5 mx-auto">
-                <div className="d-flex justify-content-center">
-                    <Title>REWIND</Title>
-                </div>
+            <Col className="d-flex justify-content-center">
+                {/*<div className="d-flex justify-content-center">*/}
+
+                {/*</div>*/}
                 <Formik
                     initialValues={{username: "", password: "", confirmPassword: ""}}
                     onSubmit={(values, actions) => {
@@ -70,15 +86,17 @@ export const RegisterPage = () => {
                     validationSchema={RegisterValidationSchema}
                 >
                     {props => {
-                        return (<Form className="myform">
+                        return (<Form className="myform shadow">
                             <div className="logo mb-3">
                                 <div className="col-md-12 text-center">
+                                    <Title>REWIND</Title>
                                     <Header>REGISTER</Header>
                                 </div>
                             </div>
                             <FormGroup className="form-group">
                                 <Label className="form-label">Username</Label>
-                                {props.touched.username && props.errors.username && <Alert variant="danger">{props.errors.username}</Alert>}
+                                {props.touched.username && props.errors.username &&
+                                <Alert variant="danger">{props.errors.username}</Alert>}
                                 <input type="text"
                                        name="username"
                                        className={"form-control " + classNames(props.touched.username
@@ -95,7 +113,8 @@ export const RegisterPage = () => {
                             </FormGroup>
                             <FormGroup className="form-group">
                                 <Label className="form-label">Password</Label>
-                                {props.touched.password && props.errors.password && <Alert variant="danger">{props.errors.password}</Alert>}
+                                {props.touched.password && props.errors.password &&
+                                <Alert variant="danger">{props.errors.password}</Alert>}
                                 <input type="password"
                                        name="password"
                                        id="password"
@@ -111,7 +130,8 @@ export const RegisterPage = () => {
                             </FormGroup>
                             <FormGroup className="form-group">
                                 <Label className="form-label">Confirm Password</Label>
-                                {props.touched.confirmPassword && props.errors.confirmPassword && <Alert variant="danger">{props.errors.confirmPassword}</Alert>}
+                                {props.touched.confirmPassword && props.errors.confirmPassword &&
+                                <Alert variant="danger">{props.errors.confirmPassword}</Alert>}
                                 <input type="password"
                                        name="confirmPassword"
                                        id="confirmPassword"
@@ -125,18 +145,24 @@ export const RegisterPage = () => {
                                        onChange={props.handleChange}
                                        placeholder="Confirm password"/>
                             </FormGroup>
-                            <FormGroup className="form-group"
-                            >
-                                <p className="text-center">By signing up you accept our <a href="#TermsOfUse">Terms
-                                    Of Use</a>
-                                </p>
-                            </FormGroup>
-                            <div className="col-md-12 text-center ">
-                                <button type="submit" className="btn-block btn-dark">Register</button>
-                            </div>
                             <FormGroup className="form-group">
-                                <p className="text-center">Already have account? <a href="#signin">Sign in here</a>
-                                </p>
+                                <p className="text-center">By signing up you accept our <a href="#TermsOfUse">Terms Of Use</a></p>
+                            </FormGroup>
+                            <FormGroup className="form-group">
+                                <div className="col-md-12 text-center ">
+                                    <Button type="submit" className="btn-block btn-dark shadow-none">Register</Button>
+                                    {/*<div className="svg-wrapper">*/}
+                                    {/*    <svg height="40" width="150" xmlns="http://www.w3.org/2000/svg">*/}
+                                    {/*        <rect id="shape" height="40" width="150"/>*/}
+                                    {/*        <div id="text">*/}
+                                    {/*            <a href=""><span className="spot"></span>Button 1</a>*/}
+                                    {/*        </div>*/}
+                                    {/*    </svg>*/}
+                                    {/*</div>*/}
+                                </div>
+                            </FormGroup>
+                            <FormGroup className="form-group">
+                                <p className="text-center">Already have account? <a href="#signin">Sign in here</a></p>
                             </FormGroup>
                         </Form>);
                     }}
