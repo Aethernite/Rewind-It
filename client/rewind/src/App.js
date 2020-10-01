3372e3e969efcf0d2afb3b67f70814f53f5248ba
Borislav Arangelov <borislav.arangelov@mentormate.com>
2020-10-01 16:45:10 +0300

    Fixed conflict in App.js

--- client/rewind/src/App.js
+++ client/rewind/src/App.js
@@ -1,22 +1,12 @@
 import React from 'react';
-<<<<<<< HEAD
 import Background from './components/Background';
 import TimesheetTable from './components/TimeSheetTable';
-=======
-import {RegisterPage} from "./components/pages/auth/RegisterPage";
-import LoginPage from "./components/pages/auth/LoginPage";
->>>>>>> e6b5fce2d1545897eea074ead5d8f553a7025017
 
 function App() {
     return (
         <div>
-<<<<<<< HEAD
             <Background />
             <TimesheetTable />
-=======
-            <LoginPage/>
-            {/*<RegisterPage/>*/}
->>>>>>> e6b5fce2d1545897eea074ead5d8f553a7025017
         </div>
     );
 }
