9f90fe04b5e03cdc600559816ddb639c537f6b8f
Aethernite <b.arangelov@mail.bg>
2020-10-18 20:08:19 +0300

    Changed queries origin from localhost to domain

--- server/frontend/src/api/AuthQueries.js
+++ server/frontend/src/api/AuthQueries.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080/api/v1',
+    baseURL: 'https://rewind-it.eu/api/v1',
     withCredentials: false,
 });
 
