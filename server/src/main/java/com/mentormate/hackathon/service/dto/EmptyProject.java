6af392c4f62048520740278f84acab21c0e5c93e
Aethernite <b.arangelov@mail.bg>
2020-10-18 13:52:40 +0300

    Packaged the FE and BE

---
+++ server/src/main/java/com/mentormate/hackathon/service/dto/EmptyProject.java
@@ -0,0 +1,10 @@
+package com.mentormate.hackathon.service.dto;
+
+public class EmptyProject {
+    private String name;
+
+    public EmptyProject(String name) {
+        this.name = name;
+    }
+    
+}
